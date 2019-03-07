4e0e39453cb5684c96da2d78e555d5896784f59a
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-03-07 10:49:42 +0530

    Create maximum no.c

---
+++ maximum no.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the num");
+scanf("%d%d%d",&a,&b,&c);
+if(a>=b&&a>=c)
+{
+print("a",a);
+}
+if else(b>=a&&b>=c)
+{
+print("b",b);
+}
+else
+{
+print("c",c);
+}
+printf("maximum");
+return 0;
+}
